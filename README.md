403f2548833d4fff9675ee3bd1321d11ae521706
anaclaragarros <acgarros@gmail.com>
2024-04-14 14:35:50 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+Projeto feito em Angular 17.0
+
+Sobre • Pré-requisitos • Tecnologias • Autor - Ana Clara Garros.
+
+✔️ Projeto CRUD Alunos 🚀 Finalizado ✔️
+💻 Sobre
+Projeto Alunos, CRUD com o fluxo natural features de um cadastro de Alunos, com recursos como:
+
+• Cadastro de alunos;
+
+• Atualização do cadastro de alunos;
+
+• Listagem de alunos;
+
+• Exclusão de alunos;
+
+Utilizei a permanëncia de informações utlizando localStorage.
+Pré-requisitos
+Antes de começar, você vai precisar ter instalado em sua máquina as seguintes ferramentas: Git, Node.js, Angular 17. Além disto é bom ter um editor para trabalhar com o código como VSCode
+
+🎲 Rodando o projeto
+# Clone este repositório
+$ git clone:
+
+# Acesse a pasta do projeto no terminal/cmd
+$ cd actuar-angular2
+
+# Instale as dependências
+$ npm install
+
+# Execute a aplicação em modo de desenvolvimento
+$ ng s
+
+# O servidor inciará na porta: http://localhost:4200/
+Integrações API
+🛠 Tecnologias
+A seguinte ferramenta foi usada na construção do projeto:
+
+Angular 17
---
+++ src/app/app.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    HttpClientModule,
+    RouterOutlet,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  private storage: Storage;
+  listagem: any;
+
+  private storageKey = "storageKey";
+
+  constructor(private http : HttpClient){
+    this.storage = window.localStorage;
+
+    this.http.get<Pessoa[]>("https://6467a872e99f0ba0a814b5ff.mockapi.io/api/Pessoas")
+           .subscribe(resultado => {
+            this.listagem = resultado;
+            console.log(resultado)
+            let storageItems = this.storage.getItem(this.storageKey);
+
+            if(storageItems != null){
+              let listagemStorage = JSON.parse(storageItems);
+              this.listagem.splice(0, this.listagem.length);
+              this.listagem.push.apply(this.listagem, listagemStorage)
+            }
+           });
+
+  }
+
+  title = 'actuar-angular-2';
+
+
+
+  adicionar() {
+    let nome =  (<HTMLInputElement>document.getElementById("name")).value;
+    let email =  (<HTMLInputElement>document.getElementById("email")).value
+    let dataNascimento =  (<HTMLInputElement>document.getElementById("dataNascimento")).value
+
+    let objeto = {
+      "Nome": nome,
+      "Email":email,
+      "DataNascimento": dataNascimento,
+      "Sexo": "F",
+    }
+
+
+    this.listagem.push(objeto);
+    this.storage.setItem(this.storageKey, JSON.stringify(this.listagem));
+  }
+
+  deletar(index: any){
+    this.listagem.splice(index, 1);
+    this.storage.setItem(this.storageKey, JSON.stringify(this.listagem));
+  }
+
+  limpar(){
+    this.storage.clear();
+    this.listagem.splice(0, this.listagem.length);
+  }
+}
+
+export class Pessoa{
+  Nome: string | null = ""
+  Email: string | null = ""
+  DataNascimento: string | null = ""
+  Sexo: string | null = ""
+}
